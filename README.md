d7343088cc80b43aeda233d10a7ec9e9ef8bd314
Jerzy Stusinski <tojastuch@gmail.com>
2024-02-04 19:30:22 +0100

    realtime enough

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 The target is to create a hardware/software combo that would allow anyone to have fun with digital processing while making it possible to use as an "effect" pedal for guitarists and other musicians. 
 
 How do we achieve that ? 
-We take rpi (3/4/5) and we add one of the [Hifi berry sound card](https://www.hifiberry.com/shop/boards/hifiberry-dac-adc/) or an [audio interface ](https://focusrite.com/products/scarlett-2i2-3rd-gen) with [low-enough latency](https://symonics.com/tested-soundcards/) and we use the [low-latency kernel raspbian build](https://github.com/guysoft/RealtimePi) to achieve <4ms input to output latency. 
+We take rpi (3/4/5) and we add one of the [Hifi berry sound card](https://www.hifiberry.com/shop/boards/hifiberry-dac-adc/) [X](https://www.hifiberry.com/docs/software/configuring-linux-3-18-x/) or an [audio interface ](https://focusrite.com/products/scarlett-2i2-3rd-gen) with [low-enough latency](https://symonics.com/tested-soundcards/) and we use the [low-latency kernel raspbian build](https://github.com/guysoft/RealtimePi) to achieve <4ms input to output latency. 
 
 # TODO
 - [X] define project structure
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -7,48 +7,30 @@
 
 
 #define SAMPLE_RATE 192000
-#define CHANNELS 2
-#define FRAMES_PER_BUFFER 64
+#define CHANNELS 1 
+#define FRAMES_PER_BUFFER 4
 
+float z = 10;
+int32_t indexx = 1;
 // Callback function for the input stream
 int inputCallback(const void *inputBuffer, void *outputBuffer,
                    unsigned long framesPerBuffer,
                    const PaStreamCallbackTimeInfo *timeInfo,
                    PaStreamCallbackFlags statusFlags,
                    void *userData) {
-    // Just copy the input data to the output
-    for (int i = 0; i < framesPerBuffer * CHANNELS; i++)
+    uint32_t *out = (uint32_t*)inputBuffer;
+    
+    for (int i = 0; i < FRAMES_PER_BUFFER; i++)
     {
-//	continue;
-    	short* a = ((short*)inputBuffer+i);
-	float* x;
-        *a /=2;
+        uint32_t* a = out + i;
+        z = fmax((int32_t)*a, z);
+        *a *= 3;
 
-
-	short cutLevel = 65536/32;
-	*a = fmax(-cutLevel, *a);
-	*a = fmin( cutLevel ,*a);
-
-
-	short makeUp = 65536/cutLevel/4;
-
-//	*a *= makeUp;
-
-
-//	printf("%d\n", *a);
-//
-	//*x = (*x/(abs(*x)+1)) * (1 - exp((*x) * (*x) / (abs(*x)+1)));
-	//*x *= 2048/4;
-	
-//	printf("%f\n", *x);
-//	*x = fmax(-65536/16, *x);
-
-//	*x = fmin(65536/16   , *x);
-
-//	*a = (short) *x;
+        //z = (*a>>31) & 0x01;
+        //indexx++;
 
     }
-    memcpy(outputBuffer, inputBuffer, framesPerBuffer * CHANNELS * sizeof(short));
+    memcpy(outputBuffer, inputBuffer, framesPerBuffer * CHANNELS * sizeof(uint32_t));
     return paContinue;
 }
 
@@ -60,37 +42,37 @@ int main() {
         std::cerr << "PortAudio initialization failed: " << Pa_GetErrorText(err) << std::endl;
         return -1;
     }
-int numDevices;
-numDevices = Pa_GetDeviceCount();
-if( numDevices < 0 )
-{
-    printf( "ERROR: Pa_CountDevices returned 0x%x\n", numDevices );
-    err = numDevices;
-    return -1;
-}
+    int numDevices;
+    numDevices = Pa_GetDeviceCount();
+    if( numDevices < 0 )
+    {
+        printf( "ERROR: Pa_CountDevices returned 0x%x\n", numDevices );
+        err = numDevices;
+        return -1;
+    }
 
-const   PaDeviceInfo *deviceInfo;
-for(int i=0; i<numDevices; i++ )
-{
-    deviceInfo = Pa_GetDeviceInfo( i );
-    std::cout<<deviceInfo->name<<std::endl;
-}
+    const   PaDeviceInfo *deviceInfo;
+    for(int i=0; i<numDevices; i++ )
+    {
+        deviceInfo = Pa_GetDeviceInfo( i );
+        std::cout<<deviceInfo->name<<std::endl;
+    }
 
 
 
     PaStream *stream;
     PaStreamParameters inputParameters, outputParameters;
 
-    inputParameters.device = 1;
+    inputParameters.device = 0;
     inputParameters.channelCount = CHANNELS;
-    inputParameters.sampleFormat = paInt16;
-    inputParameters.suggestedLatency = Pa_GetDeviceInfo(1)->defaultHighInputLatency;
+    inputParameters.sampleFormat = paInt32;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo(1)->defaultLowInputLatency;
     inputParameters.hostApiSpecificStreamInfo = nullptr;
 
-    outputParameters.device = 1; // Use the default output device for playback
+    outputParameters.device = 0; // Use the default output device for playback
     outputParameters.channelCount = CHANNELS;
-    outputParameters.sampleFormat = paInt16;
-    outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultHighOutputLatency;
+    outputParameters.sampleFormat = paInt32;
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
     outputParameters.hostApiSpecificStreamInfo = nullptr;
 
     err = Pa_OpenStream(&stream,
@@ -130,6 +112,8 @@ for(int i=0; i<numDevices; i++ )
 
     // Keep the program running until interrupted
     while (true) {
+        printf("%f\n", z);
+        z = 0;
         Pa_Sleep(100);  // Sleep for a short duration to avoid high CPU usage
     }
 
